f5befa9875c16f1dd74e558650a6b9b8867f4ee5
German Fernandez <germanfernandez0306@gmail.com>
2021-08-28 15:05:11 -0300

    ✅ add new routes, delete and post

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,3 +1,5 @@
+'use strict'
+
 import { errorUtil } from "../utils/error.js";
 
 const controller = {};
@@ -33,6 +35,24 @@ controller.getUser = (request, response) => {
         response.status(200).json(data[id]);
     } catch (error) {
         errorUtil.send(response, 400, `Error: controllers:users:getUser | ${error.message}`);
+    }
+};
+
+controller.createUser = (request, response) => {
+    try {
+        const { name, email, password } = request.body;
+        response.status(200).json({ name, email, password });
+    } catch (error) {
+        errorUtil.send(response, 400, `Error: controllers:users:createUser | ${error.message}`)
+    }
+};
+
+controller.deleteUser = (request, response) => {
+    try {
+        const { id } = request.params;
+        response.status(200).json({ id });
+    } catch (error) {
+        errorUtil.send(response, 400, `Error: controllers:users:deleteUser | ${error.message}`);
     }
 };
 
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,3 +1,5 @@
+'use strict'
+
 import express from "express";
 const router = express.Router();
 
@@ -5,5 +7,10 @@ import { controller as userController } from "../controllers/users.js";
 
 router.get("/", userController.getUsers);
 router.get("/:id", userController.getUser);
+
+router.post("/create", userController.createUser);
+
+router.delete("/:id", userController.deleteUser);
+
 
 export { router };
