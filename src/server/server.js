6450e6322d4370178381683291c9c775d34d0364
German Fernandez <germanfernandez0306@gmail.com>
2021-09-08 17:32:19 -0300

    ✅ add Mongoose

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,7 @@
 'use strict'
 
 import express from "express";
+import mongoose from "mongoose";
 import morgan from "morgan";
 
 import dotenv from "dotenv";
@@ -18,8 +19,13 @@ app.use(express.urlencoded({extended: true}));
 //Routes
 app.use(indexRouter);
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log(`SERVER LISTENING AT ${process.env.PORT || 3000}`);
+app.listen(process.env.PORT || 3000, async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB || "mongodb://localhost:27017/testing")
+        console.log(`SERVER LISTENING AT ${process.env.PORT || 3000}`);
+    } catch (error) {
+        console.log(`SERVER ERROR : ${error.message}`);
+    }
 });
 
 export { app };
