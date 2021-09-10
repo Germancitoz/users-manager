9280d010deca090f642e41fb9799a783fe2d51a8
German Fernandez <germanfernandez0306@gmail.com>
2021-09-10 19:04:43 -0300

    🎨 change routes structure

---
+++ src/routes/user.js
@@ -0,0 +1,16 @@
+"use strict";
+
+import express from "express";
+const userRouter = express.Router();
+
+import UserController from "../controllers/user.js";
+//import User from "../models/user.js";
+
+userRouter.get("/", UserController.getAll);
+userRouter.get("/:id", UserController.get);
+
+userRouter.post("/create", UserController.create);
+
+userRouter.delete("/:id", UserController.delete);
+
+export default userRouter;
