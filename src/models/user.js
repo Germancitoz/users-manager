aa0ed2b98e18aa05daa50432d36d89a4924034e4
German Fernandez <germanfernandez0306@gmail.com>
2021-09-19 21:29:35 -0300

    ✏️ fix userSshema

--- src/models/user.js
+++ src/models/user.js
@@ -5,28 +5,25 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
-        default: "Undefined",
         required: true,
     },
     email: {
         type: String,
-        default: "Undefined",
         required: true,
     },
     password: {
         type: String,
-        default: "Undefined",
         required: true,
     },
     age: {
         type: Number,
-        default: 0,
         required: true
     },
 }, {
     "toJSON" : {
         transform: (document, returnedObject) => {
             delete returnedObject.password;
+            delete returnedObject.__v;
         }
     }
 });
