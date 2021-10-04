0a088aaf4924b9e7f8db6df2d97fcf9450e2a64d
German Fernandez <germanfernandez0306@gmail.com>
2021-10-04 19:02:40 -0300

    refactor: add comments to easier work

--- src/server/index.js
+++ src/server/index.js
@@ -9,7 +9,7 @@ import userRouter from "../routes/user.js";
 
 const app = express();
 
-//Middleware
+//Middleware Basics
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan("dev"));
@@ -18,8 +18,9 @@ app.use(morgan("dev"));
 app.use("/", indexRouter);
 app.use("/user", userRouter);
 
-app.use((req, res, next) => {
-    res.status(404).json({ data: "This page not exists" });
+//Middleware Routes
+app.use((request, response, next) => {
+    response.status(404).json({ error: "Not found" });
 });
 
 export { app };
