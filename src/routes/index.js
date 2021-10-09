deec5a368a0bcff9711f9d2a169530e665218f62
Germancito <germanfernandez0306@gmail.com>
2021-10-09 15:41:51 -0300

    fix: add error to response

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,8 @@
-"use strict";
-
 import express from "express";
 const indexRouter = express.Router();
 
 indexRouter.get("/", (request, response) => {
-    response.status(200).json({ data: "Welcome to this API" });
+    response.status(200).json({ error: null, data: "Welcome to this API" });
 });
 
 export default indexRouter;
