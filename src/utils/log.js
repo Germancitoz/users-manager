ebd3bed84f08d203e366b42c56c09748c627cd02
Germancito <germanfernandez0306@gmail.com>
2021-10-18 16:43:08 -0300

    refactor: change POO to Functional

--- src/utils/log.js
+++ src/utils/log.js
@@ -1,24 +1,23 @@
 import fs from "fs";
 import path from "path";
 
-export default class Log {
-
-    /**
-     * Save a message in file.log
-     * @param  {string} message String to display
-    */
-    static save(message) {
-        fs.writeFile(path.join(path.resolve(), "/logs/file.log"), message, () => {});
-    };
-
-    /**
-     * Send a message to the console
-     * @param  {string} type Type of message
-     * @param  {string} message String to display
-    */
-    static send(type, message) {
-        const template = `[${type}] : ${message}`;
-        this.save(template);
-        console.log(template); //Only for (server console)
-    };
+/**
+ * Save a message in file.log
+ * @param  {string} message String to display
+ */
+const save = (message) => {
+  fs.writeFile(path.join(path.resolve(), "/logs/file.log"), message, () => {});
 };
+
+/**
+ * Send a message to the console
+ * @param  {string} type Type of message
+ * @param  {string} message String to display
+ */
+const send = (type, message) => {
+  const template = `[${type}] : ${message}`;
+  this.save(template);
+  console.log(template); //Only for (server console)
+};
+
+export default Log;
