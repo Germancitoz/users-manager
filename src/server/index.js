b9fcec20e186bbdea09ca54d164350901dc21eb4
Germancito <germanfernandez0306@gmail.com>
2021-10-20 16:38:58 -0300

    fix: add cors

--- src/server/index.js
+++ src/server/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import morgan from "morgan";
 
 const app = express();
@@ -6,6 +7,7 @@ const app = express();
 //Middleware Basics
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 app.use(morgan("dev"));
 
 //Routes
