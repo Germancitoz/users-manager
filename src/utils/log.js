d23dddfeaf933174041c2d03e62066f368227358
German Fernandez <germanfernandez0306@gmail.com>
2021-12-08 17:20:32 -0300

    refactor: save in object

--- src/utils/log.js
+++ src/utils/log.js
@@ -1,24 +1,19 @@
 import fs from "fs";
 import path from "path";
 
-/**
- * Save a message in file.log
- * @param  {string} message String to display
- */
-const save = (message) => {
-  fs.writeFile(path.join(path.resolve(), "/logs/file.log"), message, () => {});
-};
+const Log = {
+  save: (message) => {
+    fs.writeFile(
+      path.join(path.resolve(), "/logs/file.log"),
+      message,
+      () => {}
+    );
+  },
 
-/**
- * Send a message to the console
- * @param  {string} type Type of message
- * @param  {string} message String to display
- */
-const send = (type, message) => {
-  const template = `[${type}] : ${message}`;
-  save(template);
-  console.log(template); //Only for (server console)
+  send: (type, message) => {
+    const template = `[${type}] : ${message}`;
+    Log.save(template);
+    console.log(template); //Only for (server console)
+  },
 };
-
-const Log = { save, send };
 export default Log;
