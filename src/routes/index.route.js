571dfbeaac81da04a94c5bbd5c614d053c938398
German Fernandez <germanfernandez0306@gmail.com>
2021-12-10 12:17:10 -0300

    refactor: add next function in index route

--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -1,8 +1,7 @@
 import express from "express";
 const indexRouter = express.Router();
 
-indexRouter.get("/", (request, response) => {
-    response.status(200).json({ error: null, data: "Welcome to this API" });
+indexRouter.get("/", (request, response, next) => {
+  response.status(200).json({ error: null, data: "Welcome to this API" });
 });
-
 export default indexRouter;
